c5e2f9f9747ed8887f3a4d3ec7f435a5ad4b5bc0
mghazyfh <kichibatar@gmail.com>
2023-04-03 08:03:35 +0700

    menambahkan class TeamMember

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using modul7_Kelompok_LDI_6;
 
 DataMahasiswa1302210027 objMhs = new DataMahasiswa1302210027();
+TeamMembers1302210027 objMbr= new TeamMembers1302210027();
 
 objMhs.readJSON();
+objMbr.readJSON();
   
