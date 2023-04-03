7ff5413337f71018c3271f283b13b7dde8075cc0
bintangrayhan <bintangrayhan7@gmail.com>
2023-04-03 08:36:47 +0700

    members

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using modul7_Kelompok_LDI_6;
 
+
+
 datamahasiswa_1302213002 objmhs = new datamahasiswa_1302213002();
 teamMember_1302213002 member = new teamMember_1302213002();
 objmhs.readJSON();
 member.readJSON();
+
