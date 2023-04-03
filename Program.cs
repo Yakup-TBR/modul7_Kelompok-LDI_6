2d04bdaf85f5b0de2f1bef3e9ed4683843231b53
Yakup-TBR <127052628+Yakup-TBR@users.noreply.github.com>
2023-04-03 08:24:31 +0700

    Update Program.cs
    p

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using modul7_Kelompok_LDI_6;
+DataMahasiswa_1302210113 datamhs = new DataMahasiswa_1302210113();
+TeamMembers_1302210113 datamember = new TeamMembers_1302210113();
+GlossaryItem_1302210113 glos = new GlossaryItem_1302210113();
 
+datamhs.readJson();
+Console.WriteLine();
+datamember.readJson();
+Console.WriteLine();
+glos.readJson();
 
