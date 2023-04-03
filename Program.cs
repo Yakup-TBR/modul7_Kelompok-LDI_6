416134f2374633eb80d00f416af69fac19664cdb
bintangrayhan <bintangrayhan7@gmail.com>
2023-04-03 08:22:28 +0700

    menambahakan class team members

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using modul7_Kelompok_LDI_6;
 
 datamahasiswa_1302213002 objmhs = new datamahasiswa_1302213002();
+teamMember_1302213002 member = new teamMember_1302213002();
 objmhs.readJSON();
+member.readJSON();
---
+++ teamMember_1302213002.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_Kelompok_LDI_6
+{
+    internal class teamMember_1302213002
+    {
+        public void readJSON()
+        {
+
+            string jsonString = File.ReadAllText(@"D:\modul7_Kelompok-LDI_6\jurnal7_2_1302213002.json");
+
+            dynamic jsonObject = JsonConvert.DeserializeObject(jsonString);
+
+           
+            Console.WriteLine("daftar nama kuliah yang diambil");
+            int iter = 1;
+            foreach (var member in jsonObject.members)
+            {
+                Console.WriteLine($"{iter}{member.firstName}-{member.lastName}-{member.gender}-{member.age}-{member.nim}");
+                iter++;
+
+            }
+
+        }
+    }
+        
+}
